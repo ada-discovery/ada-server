3e75a17696692fa6ef4ba99fcd46bbac501933a5
Peter Banda <tremor11@gmail.com>
2019-10-28 12:46:06 +0100

    Inference performance improved by introducing seq 'view'.

---
+++ src/main/scala/org/ada/server/models/datatrans/FilterDataSetTransformation.scala
@@ -0,0 +1,45 @@
+package org.ada.server.models.datatrans
+
+import java.util.Date
+
+import org.ada.server.dataaccess.StreamSpec
+import org.ada.server.json.HasFormat
+import org.ada.server.models.ScheduledTime
+import org.ada.server.models.datatrans.DataSetTransformation._
+import reactivemongo.play.json.BSONFormats.BSONObjectIDFormat
+import play.api.libs.json.Json
+import reactivemongo.bson.BSONObjectID
+
+case class CopyDataSetTransformation(
+  _id: Option[BSONObjectID] = None,
+
+  sourceDataSetId: String,
+  resultDataSetSpec: ResultDataSetSpec,
+
+  streamSpec: StreamSpec,
+  scheduled: Boolean = false,
+  scheduledTime: Option[ScheduledTime] = None,
+  timeCreated: Date = new Date(),
+  timeLastExecuted: Option[Date] = None
+) extends DataSetTransformation {
+
+  override val sourceDataSetIds = Seq(sourceDataSetId)
+
+  override def copyCore(
+    __id: Option[BSONObjectID],
+    _timeCreated: Date,
+    _timeLastExecuted: Option[Date],
+    _scheduled: Boolean,
+    _scheduledTime: Option[ScheduledTime]
+  ) = copy(
+    _id = __id,
+    timeCreated = _timeCreated,
+    timeLastExecuted = _timeLastExecuted,
+    scheduled = _scheduled,
+    scheduledTime = _scheduledTime
+  )
+}
+
+object CopyDataSetTransformation extends HasFormat[CopyDataSetTransformation] {
+  val format = Json.format[CopyDataSetTransformation]
+}
---
+++ src/main/scala/org/ada/server/services/transformers/FilterDataSetTransformer.scala
@@ -0,0 +1,26 @@
+package org.ada.server.services.transformers
+
+import org.ada.server.models.datatrans.CopyDataSetTransformation
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+private class CopyDataSetTransformer extends AbstractDataSetTransformer[CopyDataSetTransformation] {
+
+  private val saveViewsAndFilters = true
+
+  override protected def execInternal(
+    spec: CopyDataSetTransformation
+  ) = {
+    val sourceDsa = dsaf(spec.sourceDataSetId).get
+
+    for {
+      // all the fields
+      fields <- sourceDsa.fieldRepo.find()
+
+      // input data stream
+      inputStream <- sourceDsa.dataSetRepo.findAsStream()
+
+    } yield
+      (sourceDsa, fields, inputStream, saveViewsAndFilters)
+  }
+}
